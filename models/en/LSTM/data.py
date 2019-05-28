a45d681e60ea013a415eb95c9a6775a790c576a1
AlexandrePsq <alexandre-pasquiou@orange.fr>
2019-05-28 16:40:25 +0200

    dictionnary functions modified

--- models/en/LSTM/data.py
+++ models/en/LSTM/data.py
@@ -17,15 +17,16 @@ class Dictionary(object):
     def __len__(self):
         return len(self.idx2word)
 
-#    def load(self, path):
-#        with open(path, 'r') as f:
-#            for line in f:
-#                self.add_word(line.rstrip('\n'))
-#
-#    def save(self, path):
-#        with open(path, 'w') as f:
-#            for w in self.idx2word:
-#                f.write('{}\n'.format(w))
+    def load(self, path):
+        with open(path, 'r') as f:
+            for line in f:
+                words = line.split() + ['<eos>']
+                for word in words:
+                    word = word.lower()
+                    self.dictionary.add_word(word)
+            self.dictionary.add_word('unk')
+
+
 
 class Corpus(object):
     def __init__(self, path):

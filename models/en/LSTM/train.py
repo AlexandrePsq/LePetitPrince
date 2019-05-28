731cb0fff14322d1e8c11dfd780a7e7899630a8f
AlexandrePsq <alexandre-pasquiou@orange.fr>
2019-05-28 16:45:32 +0200

    Split train in utils+train

--- models/en/LSTM/train.py
+++ models/en/LSTM/train.py
@@ -15,49 +15,15 @@ import time
 from data import Corpus
 from tqdm import tqdm
 from ....utilities.settings import Params
+from .utils import get_batch, repackage_hidden, batchify, save, load
 
 params = Params()
 
 
+
 ###############################################################################
-# Utilities
+# Evaluating code
 ###############################################################################
-
-
-def repackage_hidden(h):
-    """Wraps hidden states in new Tensors, to detach them from their history."""
-    if isinstance(h, torch.Tensor):
-        return h.detach()
-    else:
-        return tuple(repackage_hidden(v) for v in h)
-
-
-# get_batch subdivides the source data into chunks of length args.bptt.
-# If source is equal to the example output of the batchify function, with
-# a bptt-limit of 2, we'd get the following two Variables for i = 0:
-# ┌ a g m s ┐ ┌ b h n t ┐
-# └ b h n t ┘ └ c i o u ┘
-# Note that despite the name of the function, the subdivison of data is not
-# done along the batch dimension (i.e. dimension 1), since that was handled
-# by the batchify function. The chunks are along dimension 0, corresponding
-# to the seq_len dimension in the LSTM.
-
-def get_batch(source, i):
-    seq_len = min(params.pref.bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    return data, target
-
-
-def batchify(data, bsz, device):
-    # Work out how cleanly we can divide the dataset into bsz parts.
-    nbatch = data.size(0) // bsz
-    # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    data = data.narrow(0, 0, nbatch * bsz)
-    # Evenly divide the data across the bsz batches.
-    data = data.view(bsz, -1).t().contiguous()
-    return data.to(device)
-
 
 def evaluate(model, criterion, ntokens, data_source, eval_batch_size):
     # Turn on evaluation mode which disables dropout.
@@ -73,16 +39,6 @@ def evaluate(model, criterion, ntokens, data_source, eval_batch_size):
             hidden = repackage_hidden(hidden)
     return total_loss / (len(data_source) - 1)
 
-
-def save(model, data_name, language):
-    path = '_'.join([model.__name__, data_name, language]) + '.pt'
-    with open(path, 'wb') as f:
-        torch.save(model, f)
-
-def load(model, data_name, language):
-    path = '_'.join([model.__name__, data_name, language]) + '.pt'
-    with open(path, 'wb') as f:
-        return torch.load(f)
 
 
 ###############################################################################

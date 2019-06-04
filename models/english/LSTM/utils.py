31c55bb6c657bb668ada24b166160fd47c2f6325
AlexandrePsq <alexandre-pasquiou@orange.fr>
2019-06-04 15:24:53 +0200

    correct paths

--- models/english/LSTM/utils.py
+++ models/english/LSTM/utils.py
@@ -60,7 +60,7 @@ def save(model, data_name, language):
 
 
 def load(model, data_name, language):
-    path = '_'.join([model.__name__, data_name, language]) + '.pt'
+    path = '_'.join([model.__name__(), data_name, language]) + '.pt'
     path = os.path.join(paths.path2derivatives, 'fMRI/models', language, path)
     assert os.path.exists(path)
     with open(path, 'rb') as f:

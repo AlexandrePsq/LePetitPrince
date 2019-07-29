3fb529e32aef59ec1615312d2d5611bd19922b78
AlexandrePsq <alexandre-pasquiou@orange.fr>
2019-07-29 16:53:23 +0200

    fix bugs

--- models/english/GLOVE/utils.py
+++ models/english/GLOVE/utils.py
@@ -37,16 +37,16 @@ def concat(l, m):
 ###############################################################################
 
 
-def neighborhood_density(self, iterator, language, method='mean', threshold=0.7):
+def neighborhood_density(model, words2add, iterator, language, method='mean', threshold=0.7, param=None):
     columns_activations = ['neighborhood_density']
     activations = []
     skip = 0
     # computing metric
-    result = np.zeros(len(self.model.vocab))
-    tmp = np.zeros((len(self.model.vocab), len(self.model.vocab)))
-    for i in range(len(self.model.vocab) - 1):
-        for j in range(i + 1, len(self.model.vocab)):
-            tmp[i,j] = cosine_similarity(self.model.vectors[i], self.model.vectors[j])
+    result = np.zeros(len(model.vocab))
+    tmp = np.zeros((len(model.vocab), len(model.vocab)))
+    for i in range(len(model.vocab) - 1):
+        for j in range(i + 1, len(model.vocab)):
+            tmp[i,j] = cosine_similarity(model.vectors[i], model.vectors[j])
         vector = tmp[0,1:] if i==0 else (concat(tmp[i,i+1:], tmp[:i,i]))
         if method == 'mean':
             result[i] = np.mean(vector)
@@ -55,28 +55,28 @@ def neighborhood_density(self, iterator, language, method='mean', threshold=0.7)
             result[i] = np.count_nonzero(vector)
     # generating prediction
     for item in tqdm(iterator):
-        if item in self.words2add.keys():
-            for word in self.words2add[item][0]:
-                activations.append(result[self.model.vocab[word].index])
-            skip = self.words2add[item][1]
+        if item in words2add.keys():
+            for word in words2add[item][0]:
+                activations.append(result[model.vocab[word].index])
+            skip = words2add[item][1]
         elif skip ==0:
-            activations.append(result[self.model.vocab[word].index])
+            activations.append(result[model.vocab[word].index])
         else:
             skip -= 1
     return pd.DataFrame(np.vstack(activations), columns=columns_activations)
 
 
-def embeddings(self, iterator, language):
-    columns_activations = ['embedding-{}'.format(i) for i in range(self.param['embedding-size'])]
+def embeddings(model, words2add, iterator, language, param=None):
+    columns_activations = ['embedding-{}'.format(i) for i in range(param['embedding-size'])]
     activations = []
     skip = 0
     for item in tqdm(iterator):
-        if item in self.words2add.keys():
-            for word in self.words2add[item][0]:
-                activations.append(self.model.vectors[self.model.vocab[word]])
-            skip = self.words2add[item][1]
+        if item in words2add.keys():
+            for word in words2add[item][0]:
+                activations.append(model.vectors[model.vocab[word]])
+            skip = words2add[item][1]
         elif skip ==0:
-            activations.append(self.model.vectors[self.model.vocab[word]])
+            activations.append(model.vectors[model.vocab[word]])
         else:
             skip -= 1
     return pd.DataFrame(np.vstack(activations), columns=columns_activations)

23432855640531c981d09a59a06cd1fdd3bdab9a
AlexandrePsq <alexandre-pasquiou@orange.fr>
2019-08-28 11:50:16 +0200

    fix bugs

--- utilities/first_level_analysis.py
+++ utilities/first_level_analysis.py
@@ -213,7 +213,8 @@ def per_voxel_analysis(model, fmri_runs, design_matrices, subject, alpha_list):
                                             fmri_runs[test[0]][:,voxel].reshape((fmri_runs[test[0]].shape[0],1)), 
                                             shuffling=shuffling,
                                             n_sample=n_sample, 
-                                            alpha_percentile=params.alpha_percentile)
+                                            alpha_percentile=params.alpha_percentile,
+                                            test=True)
             scores_cv2[cv3, voxel] = r2[0]
             distribution_array[cv3, :, voxel] = distribution
 

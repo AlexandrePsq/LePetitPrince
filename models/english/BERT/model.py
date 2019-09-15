18ac7755ff84a4b66b8a441f4c567cb5e62d6d15
AlexandrePsq <alexandre-pasquiou@orange.fr>
2019-09-15 17:31:24 +0200

    finished BERT

--- models/english/BERT/model.py
+++ models/english/BERT/model.py
@@ -14,15 +14,20 @@ import numpy as np
 import torch
 from pytorch_transformers import BertTokenizer, BertModel, BertForMaskedLM, WordpieceTokenizer
 import h5py
+from utils import match_tokenized_to_untokenized, extract_activations_from_tokenized
+
+
 
 parameters = {'base':{'LAYER_COUNT':12, 'FEATURE_COUNT':768},
                 'large':{'LAYER_COUNT':24, 'FEATURE_COUNT':1024}
             }
 
+
+
 class BERT(object):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, bert_model, tokenizer, language):
+    def __init__(self, bert_model, language, name, loi):
         super(BERT, self).__init__()
         # Load pre-trained model tokenizer (vocabulary)
         # Crucially, do not do basic tokenization; PTB is tokenized. Just do wordpiece tokenization.
@@ -33,37 +38,61 @@ class BERT(object):
         self.language = language
         self.LAYER_COUNT = parameters[bert_model]['LAYER_COUNT']
         self.FEATURE_COUNT = parameters[bert_model]['FEATURE_COUNT']
+        self.name = name
+        self.generation = self.name.split('-')[2]
+        self.loi = np.array(loi) if loi else np.arange(parameters[bert_model]['LAYER_COUNT']) # loi: layers of interest
 
     def __name__(self):
-        pass
-        #return '_'.join([function.__name__ for function in self.functions])
+        return self.name
+
 
     def generate(self, path, language, textgrid):
+        """ Input text should have one sentence per line, where each word and every 
+        symbol is separated from the following by a space. No <eos> token should be included,
+        as they are automatically integrated during tokenization.
+        """
+        activations = []
         self.model.eval()
-        for line in open(path):
-            line = line.strip() # Remove trailing characters
+        if self.generation == 'bucket':
+            # Here, we give as input the text line by line.
+            for line in open(path):
+                line = line.strip() # Remove trailing characters
 
-            line = '[CLS] ' + line + ' [SEP]'
-            tokenized_text = self.tokenizer.wordpiece_tokenizer.tokenize(line)
-            indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
-            segment_ids = [1 for x in tokenized_text]
-        
-            # Convert inputs to PyTorch tensors
-            tokens_tensor = torch.tensor([indexed_tokens])
-            segments_tensors = torch.tensor([segment_ids])
-        
-            with torch.no_grad():
-                encoded_layers, _ = self.model(tokens_tensor, segments_tensors) # dimension = layer_count * len(tokenized_text) * feature_count
-        dataframes = [pd.DataFrame(function(iterator, language), columns=[function.__name__]) for function in self.functions]
-        result = pd.concat([df for df in dataframes], axis = 1)
+                line = '[CLS] ' + line + ' [SEP]'
+                tokenized_text = self.tokenizer.wordpiece_tokenizer.tokenize(line)
+                indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+                segment_ids = [1 for x in tokenized_text]
+                mapping = match_tokenized_to_untokenized(tokenized_text, line)
+
+                # Convert inputs to PyTorch tensors
+                tokens_tensor = torch.tensor([indexed_tokens])
+                segments_tensors = torch.tensor([segment_ids])
+
+                with torch.no_grad():
+                    encoded_layers, _ = self.model(tokens_tensor, segments_tensors) # dimension = layer_count * len(tokenized_text) * feature_count
+                    # filtration
+                    encoded_layers = encoded_layers[self.loi, :, :]
+                    activations += extract_activations_from_tokenized(encoded_layers.numpy(), mapping, tokenized_text)
+        elif self.generation == 'sequential':
+            # Here we give as input the sentence up to the actual word, incrementing by one at each step.
+            for line in open(path):
+                for index in range(1, len(line.split())):
+                    tmp_line = " ".join(line.split()[:index])
+                    tmp_line = tmp_line.strip() # Remove trailing characters
+
+                    tmp_line = '[CLS] ' + tmp_line + ' [SEP]'
+                    tokenized_text = self.tokenizer.wordpiece_tokenizer.tokenize(tmp_line)
+                    indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+                    segment_ids = [1 for x in tokenized_text]
+
+                    # Convert inputs to PyTorch tensors
+                    tokens_tensor = torch.tensor([indexed_tokens])
+                    segments_tensors = torch.tensor([segment_ids])
+
+                    with torch.no_grad():
+                        encoded_layers, _ = self.model(tokens_tensor, segments_tensors) # dimension = layer_count * len(tokenized_text) * feature_count
+                        # filtration
+                        encoded_layers = encoded_layers[self.loi, :, :]
+                        activations.append(np.mean(encoded_layers.numpy(), axis=1).reshape(1,-1))
+        result = pd.DataFrame(np.vstack(activations), columns=['layer-{}-{}'.format(layer, index) for layer in self.loi for index in range(self.FEATURE_COUNT)])
         return result
-
-
-  
-
-
-
-#with h5py.File(args.output_path, 'w') as fout:
-#  
-#    dset = fout.create_dataset(str(index), (LAYER_COUNT, len(tokenized_text), FEATURE_COUNT))
-#    dset[:,:,:] = np.vstack([np.array(x) for x in encoded_layers])
---
+++ models/english/BERT/utils.py
@@ -0,0 +1,41 @@
+from collections import namedtuple, defaultdict
+import os
+import numpy as np
+
+
+
+def match_tokenized_to_untokenized(tokenized_sent, untokenized_sent):
+    '''Aligns tokenized and untokenized sentence given subwords "##" prefixed
+    Assuming that each subword token that does not start a new word is prefixed
+    by two hashes, "##", computes an alignment between the un-subword-tokenized
+    and subword-tokenized sentences.
+    Args:
+        tokenized_sent: a list of strings describing a subword-tokenized sentence
+        untokenized_sent: a list of strings describing a sentence, no subword tok.
+    Returns:
+        A dictionary of type {int: list(int)} mapping each untokenized sentence
+        index to a list of subword-tokenized sentence indices
+    '''
+    mapping = defaultdict(list)
+    untokenized_sent_index = 0
+    tokenized_sent_index = 1
+    while (untokenized_sent_index < len(untokenized_sent) and tokenized_sent_index < len(tokenized_sent)):
+        while (tokenized_sent_index + 1 < len(tokenized_sent) and tokenized_sent[tokenized_sent_index + 1].startswith('##')):
+            mapping[untokenized_sent_index].append(tokenized_sent_index)
+            tokenized_sent_index += 1
+        mapping[untokenized_sent_index].append(tokenized_sent_index)
+        untokenized_sent_index += 1
+        tokenized_sent_index += 1
+    return mapping
+
+
+def extract_activations_from_tokenized(activation, mapping, tokenized_text):
+    """Take the average activations of the tokens related to a given word."""
+    nb_words = activation.shape[1]
+    new_activations = []
+    for word_index in range(nb_words):
+        word_activation = []
+        word_activation.append([tokenized_text[o] for o in mapping[word_index]])
+        word_activation = np.vstack(word_activation)
+        new_activations.append(np.mean(word_activation, axis=0).reshape(1,-1))
+    return new_activations

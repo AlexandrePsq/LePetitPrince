97c1b76238a279e7b7e9a3f25936cf617fa3aac7
AlexandrePsq <alexandre-pasquiou@orange.fr>
2019-10-16 22:22:47 +0200

    checkpoints

--- utilities/optimized_significance_clusterized.py
+++ utilities/optimized_significance_clusterized.py
@@ -1,86 +1,98 @@
 # -*- coding: utf-8 -*-
 
-import os
-import yaml
-import glob
-import argparse
-
-import warnings
-warnings.simplefilter(action='ignore')
-
-import numpy as np
-from sklearn.metrics import r2_score
-from scipy.stats import pearsonr
-import pandas as pd
-from sklearn.linear_model import Ridge
-# import h5py
-import json
-
-
-def get_score(model, y_true, x, r2_min=0., r2_max=0.99):
-    # return the R2_score for each voxel (=list)
-    prediction = model.predict(x)
-    r2 = r2_score(y_true,
-                    prediction,
-                    multioutput='raw_values')
-    pearson_corr = [pearsonr(y_true[:,i], prediction[:,i])[0] for i in range(y_true.shape[1])]
-    # remove values with are too low and values too good to be true (e.g. voxels without variation)
-    # return np.array([0 if (x < r2_min or x >= r2_max) else x for x in r2])
-    return r2, pearson_corr
-
-def check_folder(path):
-    # Create adequate folders if necessary
-    if not os.path.isdir(path):
-        check_folder(os.path.dirname(path))
-        os.mkdir(path)
-
-def update(model, parameters):
-    model.alpha = parameters['alpha']
-    model.fit_intercept = parameters['fit_intercept']
-    model.normalize = parameters['normalize']
-    model.copy_X = parameters['copy_X']
-    model.max_iter = parameters['max_iter']
-    model.tol = parameters['tol']
-    model.solver = parameters['solver']
-    model.random_state = parameters['random_state']
-    model.n_iter_ = parameters['n_iter_']
-    return model
-
-def load_model(path2folder, original_model):
-    coef_path = os.path.join(path2folder, 'coef_.npy')
-    intercept_path = os.path.join(path2folder, 'intercept_.npy')
-    parameters_path = os.path.join(path2folder, 'parameters.json')
-
-    original_model.coef_ = np.load(coef_path)
-    original_model.intercept_ = np.load(intercept_path)
-    with open(parameters_path, 'r') as outfile:
-        parameters = json.load(outfile)
-    new_model = update(original_model, parameters)
-    return new_model
-
-def save_model(path2folder, model):
-    coef_path = os.path.join(path2folder, 'coef_.npy')
-    intercept_path = os.path.join(path2folder, 'intercept_.npy')
-    parameters_path = os.path.join(path2folder, 'parameters.json')
-    parameters = vars(model).copy()
-    del parameters['coef_']
-    del parameters['intercept_']
-
-    np.save(coef_path, model.coef_)
-    np.save(intercept_path, model.intercept_)
-    with open(parameters_path, 'a+') as outfile:
-        json.dump(parameters, outfile)
-        
-
-def write(path, text):
-    with open(path, 'a+') as f:
-        f.write(text)
-        f.write('\n')
-
 
 if __name__ == '__main__':
 
     checkpoint = '/neurospin/unicog/protocols/IRMf/LePetitPrince_Pallier_2018/LePetitPrince/derivatives/fMRI/ridge-indiv/english/sub-057/checkpoint_significance.txt'
+
+    write(checkpoint, 'importing 4...')
+
+    import os
+    import yaml
+    import glob
+    import argparse
+
+    write(checkpoint, 'importing 4+2...')
+
+    import warnings
+    warnings.simplefilter(action='ignore')
+
+    write(checkpoint, 'importing 4+2+2...')
+
+    import numpy as np
+    from sklearn.metrics import r2_score
+
+    write(checkpoint, 'importing 4+2+2+4...')
+
+    from scipy.stats import pearsonr
+    import pandas as pd
+    from sklearn.linear_model import Ridge
+    # import h5py
+    import json
+
+    write(checkpoint, 'defining functions...')
+
+    def get_score(model, y_true, x, r2_min=0., r2_max=0.99):
+        # return the R2_score for each voxel (=list)
+        prediction = model.predict(x)
+        r2 = r2_score(y_true,
+                        prediction,
+                        multioutput='raw_values')
+        pearson_corr = [pearsonr(y_true[:,i], prediction[:,i])[0] for i in range(y_true.shape[1])]
+        # remove values with are too low and values too good to be true (e.g. voxels without variation)
+        # return np.array([0 if (x < r2_min or x >= r2_max) else x for x in r2])
+        return r2, pearson_corr
+
+    def check_folder(path):
+        # Create adequate folders if necessary
+        if not os.path.isdir(path):
+            check_folder(os.path.dirname(path))
+            os.mkdir(path)
+
+    def update(model, parameters):
+        model.alpha = parameters['alpha']
+        model.fit_intercept = parameters['fit_intercept']
+        model.normalize = parameters['normalize']
+        model.copy_X = parameters['copy_X']
+        model.max_iter = parameters['max_iter']
+        model.tol = parameters['tol']
+        model.solver = parameters['solver']
+        model.random_state = parameters['random_state']
+        model.n_iter_ = parameters['n_iter_']
+        return model
+
+    def load_model(path2folder, original_model):
+        coef_path = os.path.join(path2folder, 'coef_.npy')
+        intercept_path = os.path.join(path2folder, 'intercept_.npy')
+        parameters_path = os.path.join(path2folder, 'parameters.json')
+
+        original_model.coef_ = np.load(coef_path)
+        original_model.intercept_ = np.load(intercept_path)
+        with open(parameters_path, 'r') as outfile:
+            parameters = json.load(outfile)
+        new_model = update(original_model, parameters)
+        return new_model
+
+    def save_model(path2folder, model):
+        coef_path = os.path.join(path2folder, 'coef_.npy')
+        intercept_path = os.path.join(path2folder, 'intercept_.npy')
+        parameters_path = os.path.join(path2folder, 'parameters.json')
+        parameters = vars(model).copy()
+        del parameters['coef_']
+        del parameters['intercept_']
+
+        np.save(coef_path, model.coef_)
+        np.save(intercept_path, model.intercept_)
+        with open(parameters_path, 'a+') as outfile:
+            json.dump(parameters, outfile)
+            
+
+    def write(path, text):
+        with open(path, 'a+') as f:
+            f.write(text)
+            f.write('\n')
+    
+    write(checkpoint, 'parsing...')
 
     parser = argparse.ArgumentParser(description="""Objective:\nGenerate r2 maps from design matrices and fMRI data in a given language for a given model.\n\nInput:\nLanguage and models.""")
     parser.add_argument("--yaml_file", type=str, default=None, help="Path to the yaml file containing alpha, run values and the voxels associateds with.")

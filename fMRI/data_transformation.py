fe5f49f99b3673253e849cee3ca91f9694021902
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-03-20 19:04:28 +0100

    create data transformation guidelines

---
+++ fMRI/data_transformation.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Transformer(object):
+    """ Perform general transformations over a set of dataframes or numpy arrays,
+    taking into account group structures.
+    """
+    
+    
+    def __init__(self):
+        pass
+    
+    def standardize(self):
+        pass
+    
+    def scale(self):
+        pass
+        
+    def make_regressor(self):
+        pass

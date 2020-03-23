32b464938302548432b04370bde6b5318db60dd4
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-03-23 14:35:42 +0100

    Skeleton regression pipeline

--- fMRI/regression_pipeline.py
+++ fMRI/regression_pipeline.py
@@ -2,12 +2,25 @@ import os
 
 
 class Pipeline(object):
+    """ General class to pipe the different steps of the encoding analysis.
+    Allows flexible result aggregation between the functions of the defined
+    flow.
+    """
     
     def __init__(self):
         pass
     
-    def fit(self, parameters):
+    def fit(self, steps):
+        self.steps = steps
+    
+    def aggregate(self):
         pass
     
+    def distribute(self):
+        pass
+    
+    def map(self):
+        pass
+
     def compute(self, deep_representations, fmri_data, output_path, logs):
         pass

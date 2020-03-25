641a09533c50a1eda16972e6386f1293acf47c5c
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-03-25 11:59:12 +0100

    Add logger class

--- fMRI/logger.py
+++ fMRI/logger.py
@@ -8,5 +8,16 @@ class Logger(object):
     def __init__(self, path):
         self.log_path = path
     
-    def report_logs(self, logs):
+    def report_logs(self, logs, level):
+        write(self.log_path, level, end=': ')
         write(self.log_path, logs)
+    
+    def error(self, message):
+        self.report_logs(self.log_path, message, level='ERROR')
+        raise Exception(message)
+    
+    def warning(self, message):
+        self.report_logs(self.log_path, message, level='WARNING')
+
+    def info(self, message):
+        self.report_logs(self.log_path, message, level='INFO')

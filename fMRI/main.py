90291ed9de9078472d7912d554975cec9b3deaf2
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-03-26 16:51:24 +0100

    Update dependencies + add duration and offset

--- fMRI/main.py
+++ fMRI/main.py
@@ -3,7 +3,7 @@ import yaml
 import argparse
 
 
-from utilities import check_folder, read_yaml, write, get_subject_name, output_name
+from utilities import check_folder, read_yaml, write, get_subject_name, output_name, structuring_inputs
 from task import Task
 from logger import Logger
 from regression_pipeline import Pipeline
@@ -31,19 +31,11 @@ if __name__=='__main__':
     output_path = output_name(folder_path, subject, parameters['model_name'])
     masker = fetch_masker(parameters['masker_path'], parameters['language'])
     
-    # Regrouping information concerning compression of the different representations
-    indexes = []
-    compression_types = []
-    n_components_list = []
-    i = 0
-    for index, model in enumerate(parameters['models']):
-        if model['data_compression']:
-            compression_types.append(model['data_compression'])
-            n_components_list.append(model['ncomponents'])
-            indexes.append(len(eval(model['columns_to_retrieve'])))
+    # Structuring inputs for later computation
+    indexes, new_indexes, offset_type_list, duration_type_list, compression_types, n_components_list = structuring_inputs(parameters['models'])
     
     # Instanciations of the classes
-    transformer = Transformer(parameters['tr'], parameters['nscans'], parameters['hrf'])
+    transformer = Transformer(parameters['tr'], parameters['nscans'], new_indexes, offset_type_list, duration_type_list, parameters['hrf'])
     encoding_model = EncodingModel()
     splitter = Splitter(1)
     compressor = Compressor(n_components_list, indexes, compression_types)
@@ -55,13 +47,13 @@ if __name__=='__main__':
                                 name='splitter_cv_int')
     compressor_int = Task([compressor.compress], [splitter_cv_int],
                                 name='compressor_int', flatten=True) # define here the compression method wanted
-    transform_data_int = Task([transformer.standardize, transformer.make_regressor], [compressor_int],
+    transform_data_int = Task([transformer.standardize, transformer.make_regressor], [splitter_cv_int, compressor_int],
                                 name='transform_data_int', flatten=True) # functions in Task objects are read right to left
     encoding_model_int = Task([encoding_model.predict, encoding_model.fit], [splitter_cv_int, transform_data_int],
                                 name='encoding_model_int', flatten=True)
     ## Pipeline ext
     compressor_ext = Task([compressor.compress], [splitter_cv_ext, encoding_model_int], name='compressor_ext')
-    transform_data_ext = Task([transformer.standardize, transformer.make_regressor], [compressor_ext], name='transform_data_ext')
+    transform_data_ext = Task([transformer.standardize, transformer.make_regressor], [splitter_cv_ext, compressor_ext], name='transform_data_ext')
     encoding_model_ext = Task([encoding_model.predict, encoding_model.fit], [splitter_cv_ext, transform_data_ext], name='encoding_model_ext')
     
     # Creating tree structure

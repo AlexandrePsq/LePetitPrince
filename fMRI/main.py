39bf82700d731c7e742b63793ff992d0e632e3c6
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-03-26 22:49:53 +0100

    Add encoding_model instanciation + maps creation

--- fMRI/main.py
+++ fMRI/main.py
@@ -3,7 +3,7 @@ import yaml
 import argparse
 
 
-from utilities import check_folder, read_yaml, write, get_subject_name, output_name, structuring_inputs
+from utilities import check_folder, read_yaml, write, get_subject_name, output_name, structuring_inputs, aggregate_cv
 from task import Task
 from logger import Logger
 from regression_pipeline import Pipeline
@@ -30,13 +30,14 @@ if __name__=='__main__':
     subject = get_subject_name(parameters['subject'])
     output_path = output_name(folder_path, subject, parameters['model_name'])
     masker = fetch_masker(parameters['masker_path'], parameters['language'])
+    smoothed_masker = fetch_masker(parameters['smoothed_masker_path'], parameters['language'], smoothing_fwhm=5)
     
     # Structuring inputs for later computation
     indexes, new_indexes, offset_type_list, duration_type_list, compression_types, n_components_list = structuring_inputs(parameters['models'])
     
     # Instanciations of the classes
     transformer = Transformer(parameters['tr'], parameters['nscans'], new_indexes, offset_type_list, duration_type_list, parameters['hrf'])
-    encoding_model = EncodingModel()
+    encoding_model = EncodingModel(model=Ridge(), alpha=None, alpha_min_log_scale=2, alpha_max_log_scale=4, nb_alphas=25)
     splitter = Splitter(1)
     compressor = Compressor(n_components_list, indexes, compression_types)
     
@@ -49,12 +50,13 @@ if __name__=='__main__':
                                 name='compressor_int', flatten=True) # define here the compression method wanted
     transform_data_int = Task([transformer.standardize, transformer.make_regressor], [splitter_cv_int, compressor_int],
                                 name='transform_data_int', flatten=True) # functions in Task objects are read right to left
-    encoding_model_int = Task([encoding_model.predict, encoding_model.fit], [splitter_cv_int, transform_data_int],
-                                name='encoding_model_int', flatten=True)
+    encoding_model_int = Task([encoding_model.grid_search], [splitter_cv_int, transform_data_int],
+                                name='encoding_model_int', flatten=True,
+                                special_output_transform=aggregate_cv)
     ## Pipeline ext
     compressor_ext = Task([compressor.compress], [splitter_cv_ext, encoding_model_int], name='compressor_ext')
     transform_data_ext = Task([transformer.standardize, transformer.make_regressor], [splitter_cv_ext, compressor_ext], name='transform_data_ext')
-    encoding_model_ext = Task([encoding_model.predict, encoding_model.fit], [splitter_cv_ext, transform_data_ext], name='encoding_model_ext')
+    encoding_model_ext = Task([encoding_model.evaluate], [splitter_cv_ext, transform_data_ext, encoding_model_int], name='encoding_model_ext')
     
     # Creating tree structure
     splitter_cv_ext = splitter_cv_ext.set_children([splitter_cv_int, compressor_ext])
@@ -73,6 +75,23 @@ if __name__=='__main__':
     # Executing pipeline
     pipeline = Pipeline()
     pipeline.fit([splitter_cv_ext]) # retrieve the flow from children and parents dependencies
-    pipeline.compute(deep_representations, fMRI_data, output_path, logs=logs)
+    maps = pipeline.compute(deep_representations, fMRI_data, output_path, logs=logs)
+    
+    # Aggregate over cross-validation
+    maps = {key: np.mean(np.stack(np.array([dic[key] for dic in maps]), axis=0), axis=0) for key in maps[0]}
+    
+    # Plotting
+    ## R2
+    output_path = os.path.join(output_path, 'R2')
+    create_maps(masker, maps['R2'], output_path, vmax=None, logger=logs)
+    create_maps(smoothed_masker, maps['R2'], output_path, vmax=None, logger=logs)
+    ## Pearson
+    output_path = os.path.join(output_path, 'Pearson_coeff')
+    create_maps(masker, maps['Pearson_coeff'], output_path, vmax=None, logger=logs)
+    create_maps(smoothed_masker, maps['Pearson_coeff'], output_path, vmax=None, logger=logs)
+    ## Alpha (not exactly what should be done: averaging alphas)
+    output_path = os.path.join(output_path, 'alpha')
+    create_maps(masker, maps['alpha'], output_path, vmax=None, logger=logs)
+    create_maps(smoothed_masker, maps['alpha'], output_path, vmax=None, logger=logs)
     
     print("Model: {} for subject: {} --> Done".format(parameters['model_name'], subject))
